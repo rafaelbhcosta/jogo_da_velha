158ffd50a8baf423f7348a650323af8cde816e07
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-04-17 22:17:22 -0300

    att

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -1,3 +1,12 @@
 import random
+import defs
 
-computador = random.randint(1,3)
+rep = True
+
+while rep:
+    computador = random.randint(1,3)
+    defs.linha()
+    print('Olá jogador, seja bem vindo ao jogo da velha')
+    defs.linha()
+    escolha = int(input('Faça sua escolha\n1 - Pedra\n2 - Papel\n3 - Tesoura\nEscolha: '))
+    
