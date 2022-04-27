978a873648856f171112e943f7fd98b23b18a778
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-04-27 18:00:07 -0300

    Update jogo.py

--- jogo/jogo.py
+++ jogo/jogo.py
@@ -9,8 +9,40 @@ while rep:
     print('Olá jogador, seja bem vindo ao jogo da velha')
     defs.linha()
     escolha = int(input('Faça sua escolha\n1 - Pedra\n2 - Papel\n3 - Tesoura\nEscolha: '))
+    
     if computador == escolha:
         defs.linha()
         print('Resultado do jogo')
         print(f'Computador: {computador}\nVocê: {escolha}\nEMPATE')
     
+    else:
+        #Vitórias do jogador
+        if escolha == 1 and computador == 3:
+            defs.linha()
+            escolha = 'Pedra'
+            computador = 'Tesoura'
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nVocê venceu!!')
+            
+        elif escolha == 2 and computador == 1:
+            defs.linha()
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nVocê venceu!!')
+        elif escolha == 3 and computador == 2:
+            defs.linha()
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nVocê venceu!!')
+
+        #Vitórias do computador
+        elif computador == 1 and escolha == 3:
+            defs.linha()
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nO cumputador venceu!!')
+        elif computador == 2 and escolha == 1:
+            defs.linha()
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nVO cumputador venceu!!')
+        elif computador == 3 and escolha == 2:
+            defs.linha()
+            print('Resultado do jogo')
+            print(f'Computador: {computador}\nVocê: {escolha}\nO cumputador venceu!!')
