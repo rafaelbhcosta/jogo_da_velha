fe0e8706baa583057cf701642d6197235af1d909
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-04-28 10:43:51 -0300

    Update jokempo.py

--- jogo/pygame/jokempo.py
+++ jogo/pygame/jokempo.py
@@ -1,5 +1,8 @@
 import pygame
 pygame.init()
+x = 400
+y = 300
+velocidade = 10
 
 janela = pygame.display.set_mode((800,600))
 pygame.display.set_caption('Jokempo')
@@ -14,9 +17,23 @@ while abertura:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             abertura = False
+        
+        #comandos que determinam a movimentação do circulo
+        comandos = pygame.key.get_pressed()
+        if comandos[pygame.K_UP]:
+            y -= velocidade
+        if comandos[pygame.K_DOWN]:
+            y += velocidade
+        if comandos[pygame.K_RIGHT]:
+            x += velocidade
+        if comandos[pygame.K_LEFT]:
+            x -= velocidade
+
+    # Sempre que o evento FOR se repetir a tela atualiza o fundo dela para essa cor
+    janela.fill((0,0,0))
 
     # A primeira função apenas desenha um circulo na tela, a outra atualiza para o circulo aparecer
-    pygame.draw.circle(janela, (0,255,0), (400,300), 50)
+    pygame.draw.circle(janela, (0,255,0), (x,y), 50)
     pygame.display.update()
 
 pygame.quit
