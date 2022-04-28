23448b9615ef7a6d2b84d90ab3f60a4bc6b2bc08
Rafael Costa <93993679+rafaelbhcosta@users.noreply.github.com>
2022-04-28 09:14:08 -0300

    Update jokempo.py

--- jogo/pygame/jokempo.py
+++ jogo/pygame/jokempo.py
@@ -2,7 +2,7 @@ import pygame
 pygame.init()
 
 janela = pygame.display.set_mode((800,600))
-pygame.display.set_caption('Sudoku')
+pygame.display.set_caption('Jokempo')
 
 abertura = True
 while abertura:
